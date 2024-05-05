fb5c09d915600cb233c006561ac715c88127cce7
Vodculen <151860061+Vodculen@users.noreply.github.com>
2024-05-05 19:47:46 -0400

    Commented out the error in Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,14 +1,14 @@
-public class Task extends Main {
+// public class Task extends Main {
 
-    public static void taskWood() {
-        for (int totalCats = 0; totalCats <= cats.length; ++totalCats) {
-            System.out.println(cats[totalCats].catIntelligence);
-            if (cats.catIntelligence = 20) {
+//     public static void taskWood() {
+//         for (int totalCats = 0; totalCats <= cats.length; ++totalCats) {
+//             System.out.println(cats[totalCats].catIntelligence);
+//             if (cats.catIntelligence = 20) {
 
-            }
-        }
-        Thread.sleep(oneSecond);
-        System.out.println("You did it, your not a failure! ");
-    }
+//             }
+//         }
+//         Thread.sleep(oneSecond);
+//         System.out.println("You did it, your not a failure! ");
+//     }
 
-}
+// }
