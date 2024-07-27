95e95ecb866a38ff59277859731b46670c6dc22a
Vodculen <stefanaschen@gmail.com>
2024-07-27 10:32:55 -0400

    Updated with better code so the more advance programmers can understand

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
-        // Health values can be inbetween 30 and 50, while, everyother stat is inbetween
-        // 0 and 20
+        Scanner usersInput = new Scanner(System.in);
+        // Health values can be inbetween 30 and 50, while, everyother stat is inbetween 0 and 20
 
         Cat jax = new Cat("Jax", 50, 20, 20, 20);
         Cat ali = new Cat("Ali", 50, 20, 20, 20);
@@ -35,104 +33,97 @@ public class Main {
         Cat grey = new Cat("Grey", 50, 20, 20, 20);
 
         Cat cats[] = { jax, ali, macaroni, zelda, aurora, charlie, mocha, latte, stella, luna, peanut, tommy, hilda,
-                hazel, sammy, curry, spots, meggy, linus, kelder, zima, fluffy, trees, grey };
+            hazel, sammy, curry, spots, meggy, linus, kelder, zima, fluffy, trees, grey };
 
-        for (int totalCats = 0; totalCats <= cats.length; ++totalCats) {
+        for (int totalCats = 0; totalCats <= cats.length - 1; ++totalCats) {
             System.out.println(cats[totalCats].prettyString());
-        }
-
+        }    
         List<Cat> party = new ArrayList<Cat>();
+        userChooseCat(cats, usersInput);
+    }
 
+    private static void wholeStartingMenuConfirmation(Cat[] cats, List<Cat> party) {
         Scanner usersInput = new Scanner(System.in);
         Cat userChoseThisCat;
         String fullStartingMenuYOrN;
+        String townName = usersChoosesTown(usersInput);
 
-        do {
+        userChoseThisCat = userChooseCat(cats, usersInput);
+        party.add(userChoseThisCat);
 
-            userChoseThisCat = userChooseCat(cats, usersInput);
-            party.add(userChoseThisCat);
+        System.out.print("Do you want to start as " + userChoseThisCat.catName + " and call your town " + townName + "? Y or N: ");
 
-            String denName = usersChoosesTown(usersInput);
+        fullStartingMenuYOrN = usersInput.nextLine();
 
-            System.out.print("Do you want to start as " + userChoseThisCat.catName + " and call your town " + denName
-                    + "? Y or N: ");
-            fullStartingMenuYOrN = usersInput.nextLine();
-
-            switch (fullStartingMenuYOrN) {
-                case "y":
-                case "Y":
-
-                    break;
-                case "n":
-                case "N":
-
-                    break;
-
-                default:
-                    System.out.println("Invalid choice. Please enter Y or N.");
-                    break;
-
-            }
-
+        if (fullStartingMenuYOrN.equalsIgnoreCase("Yes") || fullStartingMenuYOrN.equalsIgnoreCase("Y")) {
+            System.out.println("Works");
+        } 
+        if (fullStartingMenuYOrN.equalsIgnoreCase("No") || fullStartingMenuYOrN.equalsIgnoreCase("N")) {
+            userChooseCat(cats, usersInput);
         }
-
-        while (fullStartingMenuYOrN != null
-                && (fullStartingMenuYOrN.equalsIgnoreCase("N") || fullStartingMenuYOrN.equalsIgnoreCase("n")));
-
-        String taskChoice = usersInput.nextLine();
-
-        switch (taskChoice) {
-            case "Wood":
-                Task.taskWood();
-                break;
-
-            default:
-                break;
+        else {
+            System.out.println("Invalid choice! Please enter Y or N.");
+            wholeStartingMenuConfirmation(cats, party);
         }
-
     }
-
+    
     private static String usersChoosesTown(Scanner usersInput) {
-        String denChoiceYOrN;
+        String townChoiceYOrN;
         String catTownName;
 
-        do {
-            System.out.print("What do you want to call your town: ");
-            catTownName = usersInput.nextLine();
-            System.out.print("Do you want to call your Town " + catTownName + "? Y or N: ");
-            denChoiceYOrN = usersInput.nextLine();
+        System.out.print("What do you want to call your town: ");
+        catTownName = usersInput.nextLine();
+        System.out.print("Do you want to call your Town " + catTownName + "? Y or N: ");
+        townChoiceYOrN = usersInput.nextLine();
 
-        } while (denChoiceYOrN != null && (denChoiceYOrN.equalsIgnoreCase("N") || denChoiceYOrN.equalsIgnoreCase("n")));
+        if (townChoiceYOrN.equalsIgnoreCase("Yes") || townChoiceYOrN.equalsIgnoreCase("Y")) {
+            //TODO: wholeStartingMenuConfirmation(cats, party);
+            //Error given cannot be solved with certainty
+        }
+        if (townChoiceYOrN.equalsIgnoreCase("No") || townChoiceYOrN.equalsIgnoreCase("N")) {
+            usersChoosesTown(usersInput);
+        }
+        else {
+            System.out.println("Invalid choice! Please enter Y or N.");
+            usersChoosesTown(usersInput);
+        }
+
         return catTownName;
     }
 
-    // List all available cats and let user choose one.
-    // Get user confirmation, if no, list cats again.
-    // returns the cat
     private static Cat userChooseCat(Cat[] cats, Scanner usersInput) {
         String menuChoice;
         Cat userChoseThisCat;
         String menuChoiceYOrN;
-        do {
 
-            System.out.println("Select One Cat:");
-            for (int i = 0; i < cats.length; i++) {
-                System.out.println(i + "> " + cats[i].catName);
-            }
-            System.out.println();
+        System.out.println("Select One Cat:");
+        for (int i = 0; i < cats.length; i++) {
+            System.out.println(i + "> " + cats[i].catName);
+        }
+        System.out.println();
 
-            System.out.print("Selected Cat: ");
-            // menuChoice is a String containing the index of the cats array
+        System.out.print("Selected Cat: ");
+        // menuChoice is a String containing the index of the cats array
 
-            String userData = usersInput.nextLine(); // the string user typed in
-            int userDataInt = Integer.parseInt(userData); // convert the user's input into an integer
-            userChoseThisCat = cats[userDataInt];
-            menuChoice = userChoseThisCat.catName;
+        String userData = usersInput.nextLine(); // the string user typed in
+        int userDataInt = Integer.parseInt(userData); // convert the user's input into an integer
+        userChoseThisCat = cats[userDataInt];
+        menuChoice = userChoseThisCat.catName;
 
-            System.out.print("Do you want to be " + menuChoice + ", Y or N: ");
-            menuChoiceYOrN = usersInput.nextLine();
+        System.out.print("Do you want to be " + menuChoice + ", Y or N: ");
 
-        } while (menuChoiceYOrN != null && (menuChoiceYOrN.equalsIgnoreCase("N")));
+        menuChoiceYOrN = usersInput.nextLine();
+
+        if (menuChoiceYOrN.equalsIgnoreCase("Yes") || menuChoiceYOrN.equalsIgnoreCase("Y")) {
+            usersChoosesTown(usersInput);
+        }
+        if (menuChoiceYOrN.equalsIgnoreCase("No") || menuChoiceYOrN.equalsIgnoreCase("N")) {
+            userChooseCat(cats, usersInput);
+        }
+        else {
+            System.out.println("Invalid choice! Please enter Y or N.");
+            userChooseCat(cats, usersInput);
+        }
 
         return userChoseThisCat;
     }
